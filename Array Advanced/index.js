3bdee7986c0437fa02f4c32d0d6cb93c3f44a55c
Lucas <lucasdecastrodev@gmail.com>
2023-09-29 19:50:17 -0300

    Learning JavaScript

---
+++ Array Advanced/index.js
@@ -0,0 +1,20 @@
+// Valor por referência
+// const nomes = ["Lucas", "Ana", "Maria"];
+// const nomes = new Array("Lucas", "Eduardo", "Antônio");
+// const novo = [...nomes];
+
+// novo.pop();
+
+// nomes.push("Teste");
+// nomes.unshift("Teste");
+
+// const novo = nomes.slice(1, 3);
+
+// nomes[2] = "João";
+// delete nomes[2];
+// console.log(novo);
+
+const nomes = [ "Lucas", "Antunes", "de", "Castro" ];
+// const nomes = nome.split(" ");
+const nome = nomes.join(" ");
+console.log(nome);
